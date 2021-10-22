1350d2ea639b506c1c86e7dc93776b15b8b5c583
Jielin-Code <68259621+Jielin-Code@users.noreply.github.com>
2021-10-22 20:12:46 +0800

    Update WJLHA3.h

--- WJLHA3/WJLHA3.h
+++ WJLHA3/WJLHA3.h
@@ -1,7 +1,7 @@
 /**************************************************************************
  * Based on Weighted Probability Model Code(Jielin Code) And <Hash Algorithm with Adaptive Hash Value Length Based on Weighted Probability Model>
  * @author JieLin Wang(China)
- * @copyright JieLin Wang 2021-05-26
+ * @copyright JieLin Wang 2021-10-22
  * @Version 3.0.0
  * @email 254908447@qq.com
  */
@@ -15,11 +15,9 @@
 the main Wang Jie lin hash function
 InBytesBuFF: the first address of bytes cache waiting to be encoding.
 InBytesBuFF_Length: the bytes cache length.
-keytBuFF: the keyt bytes for encoding.
-keytBuFF_Length:the keytBuFF's byte length.
 HashValueBuFF:the hash value Byte Buff.
 HashValueBuFF_Length: the hash value's byte length, by user-defined or system-defined.
 *************************************************************************************/
-void WJLHA3(unsigned char *InBytesBuFF, int InBytesBuFF_Length, unsigned char *keytBuFF, int keytBuFF_Length, unsigned char *HashValueBuFF, int HashValueBuFF_Length);
+void WJLHA3(unsigned char *InBytesBuFF, int InBytesBuFF_Length, unsigned char *HashValueBuFF, int HashValueBuFF_Length);
 
 #endif
